b1f991795c50f6cc2f2a3a29c1842471b925cf8d
nicksen35 <nicksen223@gmail.com>
2022-12-23 17:39:34 +0800

    Basic TTT Logic

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public Text[] spaceList;
+<<<<<<< HEAD
     
     public string GetSide = "X";
 
@@ -14,16 +15,87 @@ public class GameManager : MonoBehaviour
         if (GetSide == "X")
         {
             GetSide = "0";
+=======
+    private int moves;
+    public string side;
+    
+    void Start()
+    {
+        SetGameControllerReferenceForButtons();
+        side = "X";
+        moves = 0;
+    }
+
+    // Update is called once per frame
+    void SetGameControllerReferenceForButtons()
+    {
+	for (int i = 0; i < spaceList.Length; i++)
+    {
+        spaceList[i].GetComponentInParent<GridSpace>().SetControllerReference(this);
+    }
+		
+    }
+
+    
+    
+
+    public void SwitchSides() 
+    {
+        if (side == "X")
+        {
+            side = "O";
+>>>>>>> 00ee2f9 (Basic Logic)
         }
             
         else
         {
+<<<<<<< HEAD
             GetSide = "X";
+=======
+            side = "X";
+>>>>>>> 00ee2f9 (Basic Logic)
         }
     }
 
     public void EndTurn() 
     {
+<<<<<<< HEAD
 
+=======
+        moves++;
+        if (spaceList[0].text == side && spaceList[1].text == side && spaceList[2].text == side)
+        {
+            UnityEngine.Debug.Log("You Win");
+        }
+        else if (spaceList[3].text == side && spaceList[4].text == side && spaceList[5].text == side)
+        {
+        }
+        else if  (spaceList[6].text == side && spaceList[7].text == side && spaceList[8].text == side)
+        {
+
+        }
+        else if (spaceList[0].text == side && spaceList[3].text == side && spaceList[6].text == side)
+        {
+
+        }
+        else if(spaceList[1].text == side && spaceList[4].text == side && spaceList[7].text == side)
+        {
+
+        }
+        else if (spaceList[0].text == side && spaceList[4].text == side && spaceList[8].text == side)
+        {
+
+        }
+        else if (spaceList[2].text == side && spaceList[4].text == side && spaceList[6].text == side)
+        if (moves >= 9)
+        {
+            UnityEngine.Debug.Log("Too many moves");
+        }
+        SwitchSides();
+    }
+    public string GetSide()
+    {   
+        return side;
+>>>>>>> 00ee2f9 (Basic Logic)
     }
 }
--- Assets/GridSpace.cs
+++ Assets/GridSpace.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< HEAD
 
 public class GridSpace : MonoBehaviour
 {
@@ -18,4 +19,31 @@ public class GridSpace : MonoBehaviour
     {
         
     }
+=======
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;
+    public Text buttontext;
+    private GameManager gm;
+
+    void Start()
+    {
+    }
+    // Start is called before the first frame update
+       public void SetControllerReference(GameManager manager)
+    {
+	    gm = manager;
+    }
+
+    public void SpaceSet() 
+    {
+        UnityEngine.Debug.Log("Hello"); 
+        buttontext.text = gm.GetSide(); 
+        button.interactable = false;
+        gm.EndTurn();
+    }
+ 
+>>>>>>> 00ee2f9 (Basic Logic)
 }
