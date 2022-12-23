6fe8e87547d25cf20a2424a9a73f54f52cad9518
nicksen35 <61608696+nicksen35@users.noreply.github.com>
2022-12-23 22:42:31 +0800

    Update GridSpace.cs
    updated ui error

--- Assets/GridSpace.cs
+++ Assets/GridSpace.cs
@@ -1,36 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
-
+using UnityEngine.UI;
 public class GridSpace : MonoBehaviour
 {
     private static GameManager gm;
-
+    public Button button;
+    public Text buttontext;
 
     // Start is called before the first frame update
     void Start()
     {
-        gm = FindObjectOfType<GameManager>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-=======
-using UnityEngine.UI;
-
-public class GridSpace : MonoBehaviour
-{
-    public Button button;
-    public Text buttontext;
-    private GameManager gm;
-
-    void Start()
-    {
-    }
     // Start is called before the first frame update
        public void SetControllerReference(GameManager manager)
     {
@@ -45,5 +27,4 @@ public class GridSpace : MonoBehaviour
         gm.EndTurn();
     }
  
->>>>>>> 00ee2f9 (Basic Logic)
 }
